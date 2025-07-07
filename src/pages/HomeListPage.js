f6902679d6c3c4ed93dc2848354fe28b1602e878
ozdemirilayda <ozdemirilayda95@gmail.com>
2025-07-07 20:07:24 +0300

    Update by Table

--- src/pages/HomeListPage.js
+++ src/pages/HomeListPage.js
@@ -3,16 +3,18 @@ import {
   html,
   css,
 } from "https://cdn.jsdelivr.net/gh/lit/dist@3/core/lit-core.min.js";
-
+import "../components/TableEmployee.js";
 export class HomeListPage extends LitElement {
   static styles = css`
     div {
-      color: red;
-      background-color: gray;
     }
   `;
   render() {
-    return html` <div>HomeListPage page</div> `;
+    return html`
+      <sub-header-page>
+        <div><table-employee></table-employee></div>
+      </sub-header-page>
+    `;
   }
 }
 customElements.define("home-list-page", HomeListPage);
