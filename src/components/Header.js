80ec4e8f30182e17a78f2bd18d570cb79aca55c9
ozdemirilayda <ozdemirilayda95@gmail.com>
2025-07-16 17:35:17 +0300

    Add language

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,6 @@ import {
   css,
 } from "https://cdn.jsdelivr.net/gh/lit/dist@3/core/lit-core.min.js";
 import { Router } from "https://cdn.jsdelivr.net/npm/@vaadin/router/+esm";
-
 export class Header extends LitElement {
   static properties = {
     language: { type: String },
@@ -22,27 +21,41 @@ export class Header extends LitElement {
       background-color: white;
       color: #ff6101;
       z-index: 10000;
-      nav {
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-        span {
-          margin-left: 1rem;
-          color: #ff6201b2;
-          display: flex;
-          align-items: center;
-          cursor: pointer;
-          &:hover {
-            color: #ff6101;
-          }
-        }
-      }
+    }
+    header nav {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+    }
+    header nav span {
+      margin-left: 1rem;
+      color: #ff6201b2;
+      display: flex;
+      align-items: center;
+      cursor: pointer;
+      user-select: none;
+      transition: color 0.3s ease;
+    }
+    header nav span:hover {
+      color: #ff6101;
     }
   `;
   constructor() {
     super();
     this.language = localStorage.getItem("lang") || "en";
   }
+  connectedCallback() {
+    super.connectedCallback();
+    window.addEventListener("language-changed", this._onLanguageChanged);
+  }
+  disconnectedCallback() {
+    super.disconnectedCallback();
+    window.removeEventListener("language-changed", this._onLanguageChanged);
+  }
+  _onLanguageChanged = (e) => {
+    this.language = e.detail;
+    this.requestUpdate();
+  };
   navigateTo(path) {
     Router.go(path);
   }
@@ -56,30 +69,36 @@ export class Header extends LitElement {
     );
   }
   render() {
-    return html` <header>
-      <div class="logo" @click=${() => this.navigateTo("/")}>
-        <img src="./logo.png" alt="logo" />
-      </div>
-      <nav>
-        <span @click=${() => this.navigateTo("/")}>
-          <iconify-icon icon="mdi:headset" width="24" height="24"></iconify-icon
-          >Employees</span
-        >
-        <span @click=${() => this.navigateTo("/add")}>
-          <iconify-icon icon="mdi:plus" width="24" height="24"></iconify-icon
-          >Add New</span
-        >
-        <span @click=${this.toggleLanguage}>
-          <iconify-icon
-            icon=${this.language === "en"
-              ? "twemoji:flag-turkey"
-              : "twemoji:flag-united-kingdom"}
-            width="24"
-            height="24"
-          ></iconify-icon>
-        </span>
-      </nav>
-    </header>`;
+    return html`
+      <header>
+        <div class="logo" @click=${() => this.navigateTo("/")}>
+          <img src="./logo.png" alt="logo" />
+        </div>
+        <nav>
+          <span @click=${() => this.navigateTo("/")}>
+            <iconify-icon
+              icon="mdi:headset"
+              width="24"
+              height="24"
+            ></iconify-icon
+            >Employees
+          </span>
+          <span @click=${() => this.navigateTo("/add")}>
+            <iconify-icon icon="mdi:plus" width="24" height="24"></iconify-icon
+            >Add New
+          </span>
+          <span @click=${this.toggleLanguage}>
+            <iconify-icon
+              icon=${this.language === "en"
+                ? "twemoji:flag-united-kingdom"
+                : "twemoji:flag-turkey"}
+              width="24"
+              height="24"
+            ></iconify-icon>
+          </span>
+        </nav>
+      </header>
+    `;
   }
 }
 customElements.define("ing-header", Header);
--- src/components/TableList/TableHeader.js
+++ src/components/TableList/TableHeader.js
@@ -4,14 +4,29 @@ import {
   css,
 } from "https://cdn.jsdelivr.net/gh/lit/dist@3/core/lit-core.min.js";
 import "../CheckboxComponent.js";
+import { t } from "../../i18n.js";
 export class TableHeader extends LitElement {
   static properties = {
     checkedAll: { type: Boolean },
+    language: { type: String },
   };
   constructor() {
     super();
-    this.selectAll = false;
+    this.checkedAll = false;
+    this.language = localStorage.getItem("lang") || "en";
   }
+  connectedCallback() {
+    super.connectedCallback();
+    window.addEventListener("language-changed", this._onLanguageChanged);
+  }
+  disconnectedCallback() {
+    super.disconnectedCallback();
+    window.removeEventListener("language-changed", this._onLanguageChanged);
+  }
+  _onLanguageChanged = (e) => {
+    this.language = e.detail;
+    this.requestUpdate();
+  };
   _toggleAll(e) {
     const checked = e.detail.checked;
     this.checkedAll = checked;
@@ -59,17 +74,19 @@ export class TableHeader extends LitElement {
           .checked=${this.checkedAll}
           @checkbox-changed=${this._toggleAll}
         ></checkbox-component>
-        <div>First Name</div>
-        <div>Last Name</div>
-        <div>Date of Employment</div>
-        <div>Date of Birth</div>
-        <div>Phone</div>
+        <div>${t("form.name", this.language)}</div>
+        <div>${t("form.surname", this.language)}</div>
+        <div>${t("form.doe", this.language)}</div>
+        <div>${t("form.birthday", this.language)}</div>
+        <div>${t("form.phone", this.language)}</div>
         <div>Email</div>
-        <div>Department</div>
-        <div>Position</div>
-        <div>Actions</div>
+        <div>${t("form.department", this.language)}</div>
+        <div>${t("form.position", this.language)}</div>
+        <div>${t("form.actions", this.language)}</div>
       </div>
     `;
   }
 }
+
 customElements.define("table-header", TableHeader);
+twemoji: flag - united - kingdom;
